5a4cd32cf9d1364fb82f8d98af464374d2e39013
DesG01211517 <desmongarrison2@gmail.com>
2024-05-21 22:10:32 -0400

    testing commit

--- modules/arrayUtils.js
+++ modules/arrayUtils.js
@@ -1,5 +1,9 @@
 const _ = require("lodash");
 
 function chunkArray(array, chunkSize) {
-  return;
+  return _.chunk(array, chunkSize);
 }
+function findUniqueElements(array, findUniqueElements) {
+  return _.findUniqueElements(array, findUniqueElements);
+}
+module.exports = { chunkArray, findUniqueElements };
